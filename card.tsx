efda01319df95d0f646eab9d2e3961821f5929b0
tamufreshmarket <harrykuhudzehwe@yahoo.com>
2025-06-05 11:17:54 +0200

    Delete card.tsx

--- card.tsx
+++
@@ -1,7 +0,0 @@
-export const Card = ({ children, className }) => (
-  <div className={`bg-white rounded-lg border ${className}`}>{children}</div>
-);
-
-export const CardContent = ({ children, className }) => (
-  <div className={className}>{children}</div>
-);
