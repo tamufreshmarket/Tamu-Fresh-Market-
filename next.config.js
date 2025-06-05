0f59fe5fd997dbb01d3dfdbcdd52879cdaa5891e
tamufreshmarket <harrykuhudzehwe@yahoo.com>
2025-06-05 15:56:01 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
-// src/next.config.js
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: [], // add any external image domains here if needed
+  experimental: {
+    appDir: true,
   },
 };
 
