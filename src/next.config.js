61b1bffa865b4d47201e97ceb7a7dbe3b5f76610
tamufreshmarket <harrykuhudzehwe@yahoo.com>
2025-06-05 15:46:47 +0200

    Create next.config.js

---
+++ src/next.config.js
@@ -0,0 +1,11 @@
+// src/next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [], // add any external image domains here if needed
+  },
+};
+
+module.exports = nextConfig;
